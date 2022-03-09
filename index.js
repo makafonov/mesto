8160b4fb669d5aad06654e3bcfa951b8394974e3
Denis S <makafonov@gmail.com>
2022-03-09 16:15:13 +0500

    Исправлена разметка формы редактирования профиля

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const editButton = document.querySelector('.button_type_edit');
 const closeButton = document.querySelector('.button_type_close');
 const popup = document.querySelector('.popup');
-const form = document.querySelector('form');
-const [nameInput, descriptionInput] = document.querySelectorAll('input');
+const userProfileForm = document.forms.userProfile;
+const nameInput = userProfileForm.querySelector('.popup__input_property_name');
+const descriptionInput = userProfileForm.querySelector('.popup__input_property_description');
 const userName = document.querySelector('.profile__title');
 const userDescription = document.querySelector('.profile__description');
 
@@ -10,15 +11,19 @@ function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
 
-closeButton.addEventListener('click', togglePopup);
-editButton.addEventListener('click', () => {
+function editProfile() {
   nameInput.value = userName.textContent;
   descriptionInput.value = userDescription.textContent;
   togglePopup();
-});
-form.addEventListener('submit', (event) => {
+}
+
+function submitProfileForm(event) {
   event.preventDefault();
   userName.textContent = nameInput.value;
   userDescription.textContent = descriptionInput.value;
   togglePopup();
-});
+}
+
+closeButton.addEventListener('click', togglePopup);
+editButton.addEventListener('click', editProfile);
+userProfileForm.addEventListener('submit', submitProfileForm);
