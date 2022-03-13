3c802b1dfca80993d61cf925dea80218cbc9d8c8
Denis S <makafonov@gmail.com>
2022-03-13 10:29:40 +0500

    Реализовано добавление карточки

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const userProfileForm = document.forms.userProfile;
 const nameInput = userProfileForm.querySelector('.popup__input_property_name');
 const descriptionInput = userProfileForm.querySelector('.popup__input_property_description');
 
+const addCardForm = document.forms.addCard;
+
 const userName = document.querySelector('.profile__title');
 const userDescription = document.querySelector('.profile__description');
 const galleryContainer = document.querySelector('.gallery');
@@ -16,31 +18,31 @@ const galleryContainer = document.querySelector('.gallery');
 const initialCards = [
   {
     name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
   },
   {
     name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
   },
   {
     name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
   },
   {
     name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
   },
   {
     name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
   },
   {
     name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
 ];
 
-const addCard = (name, link) => {
+const addCard = (name, link, first = true) => {
   const cardTemplate = document.querySelector('#gallery-item-template').content;
   const cardElement = cardTemplate.querySelector('.gallery__item').cloneNode(true);
 
@@ -48,29 +50,35 @@ const addCard = (name, link) => {
   cardElement.querySelector('.gallery__image').alt = `${name}.`;
   cardElement.querySelector('.gallery__image').src = link;
 
-  galleryContainer.append(cardElement);
+  if (first) {
+    galleryContainer.prepend(cardElement);
+  } else {
+    galleryContainer.append(cardElement);
+  }
 };
-
-const renderCards = () => {
-  initialCards.forEach((card) => {
-    addCard(card.name, card.link);
-  })
-}
-
-renderCards();
 
 const togglePopup = (popup) => popup.classList.toggle('popup_opened');
 const editProfile = () => {
   nameInput.value = userName.textContent;
   descriptionInput.value = userDescription.textContent;
   togglePopup(popupEditProfile);
-}
+};
 const submitProfileForm = (event) => {
   event.preventDefault();
   userName.textContent = nameInput.value;
   userDescription.textContent = descriptionInput.value;
   togglePopup(popupEditProfile);
-}
+};
+const submitCardForm = (event) => {
+  event.preventDefault();
+  const cardName = addCardForm.querySelector('.popup__input_property_name').value;
+  const cardLink = addCardForm.querySelector('.popup__input_property_link').value;
+  addCard(cardName, cardLink);
+  addCardForm.reset();
+  togglePopup(popupAddCard);
+};
+
+initialCards.forEach((card) => addCard(card.name, card.link, false));
 
 closeButtons.forEach((button) => {
   button.addEventListener('click', (event) => togglePopup(event.target.closest('.popup')));
@@ -78,3 +86,4 @@ closeButtons.forEach((button) => {
 addCardButton.addEventListener('click', () => togglePopup(popupAddCard));
 editButton.addEventListener('click', editProfile);
 userProfileForm.addEventListener('submit', submitProfileForm);
+addCardForm.addEventListener('submit', submitCardForm);
