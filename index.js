b8037a65c458f52938943d5491f783d1c9f8028b
Denis S <makafonov@gmail.com>
2022-03-13 10:01:57 +0500

    Форма добавления карточки

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const editButton = document.querySelector('.button_type_edit');
-const closeButton = document.querySelector('.button_type_close');
-const popup = document.querySelector('.popup');
+const addCardButton = document.querySelector('.button_type_add');
+const closeButtons = document.querySelectorAll('.button_type_close');
+
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddCard = document.querySelector('.popup_type_add-card');
+
 const userProfileForm = document.forms.userProfile;
 const nameInput = userProfileForm.querySelector('.popup__input_property_name');
 const descriptionInput = userProfileForm.querySelector('.popup__input_property_description');
+
 const userName = document.querySelector('.profile__title');
 const userDescription = document.querySelector('.profile__description');
 const galleryContainer = document.querySelector('.gallery');
@@ -54,23 +59,22 @@ const renderCards = () => {
 
 renderCards();
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
-}
-
-function editProfile() {
+const togglePopup = (popup) => popup.classList.toggle('popup_opened');
+const editProfile = () => {
   nameInput.value = userName.textContent;
   descriptionInput.value = userDescription.textContent;
-  togglePopup();
+  togglePopup(popupEditProfile);
 }
-
-function submitProfileForm(event) {
+const submitProfileForm = (event) => {
   event.preventDefault();
   userName.textContent = nameInput.value;
   userDescription.textContent = descriptionInput.value;
-  togglePopup();
+  togglePopup(popupEditProfile);
 }
 
-closeButton.addEventListener('click', togglePopup);
+closeButtons.forEach((button) => {
+  button.addEventListener('click', (event) => togglePopup(event.target.closest('.popup')));
+});
+addCardButton.addEventListener('click', () => togglePopup(popupAddCard));
 editButton.addEventListener('click', editProfile);
 userProfileForm.addEventListener('submit', submitProfileForm);
