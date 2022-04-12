daea7b3e02ffba63189f3bc3eacd55ddaf306169
Denis S <makafonov@gmail.com>
2022-04-12 20:53:56 +0500

    Добавлен класс карточки

---
+++ scripts/Card.js
@@ -0,0 +1,50 @@
+export default class {
+  constructor(data, cardSelector) {
+    this._cardSelector = cardSelector;
+    this._name = data.name;
+    this._link = data.link;
+
+    this._preview = data.preview;
+  }
+
+  _getTemplate() {
+    return document
+      .querySelector(this._cardSelector)
+      .content.querySelector('.gallery__item')
+      .cloneNode(true);
+  }
+
+  _toggleLike(event) {
+    event.target.classList.toggle('button_type_like-active');
+  }
+
+  _remove(event) {
+    event.target.closest('.gallery__item').remove();
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.button_type_like').addEventListener('click', this._toggleLike);
+    this._element.querySelector('.button_type_trash').addEventListener('click', this._remove);
+    this._imageElement.addEventListener('click', () => this._preview(this));
+  }
+
+  getName() {
+    return this._name;
+  }
+
+  getLink() {
+    return this._link;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._imageElement = this._element.querySelector('.gallery__image');
+    this._setEventListeners();
+
+    this._element.querySelector('.gallery__title').textContent = this._name;
+    this._imageElement.alt = this._name;
+    this._imageElement.src = this._link;
+
+    return this._element;
+  }
+}
